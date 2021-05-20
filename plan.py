94178cc89e13a2059fb14ada0f86903cd31325d2
Dr. Klauz <84529194+DuoAnDi2018@users.noreply.github.com>
2021-05-20 22:12:48 +0530

    Fix info exposure

--- plan.py
+++ plan.py
@@ -2,7 +2,10 @@ print("Here is my evil plan!")
 print("I will take over the world with my mind controlinator!")
 print("Although on second thoughts I had a secret passphrase made just in case I needed to scrape the plan")
 password = input("Do you know the phrase? Enter: ")
-if (password=="abort12345"):
+# just a placeholder not the real passphrase
+redacted = *******
+# removed the publicly visible passphrase
+if (password==redacted):
     print("Plan aborted! Here is your flag!")
 else:
     print("The plan must go on!")
