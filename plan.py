0aff09ef9d7288e88b923cf26dee6fad287f7d2e
Dr. Klauz <84529194+DuoAnDi2018@users.noreply.github.com>
2021-05-20 22:08:49 +0530

    Initial evil plan

---
+++ plan.py
@@ -0,0 +1,8 @@
+print("Here is my evil plan!")
+print("I will take over the world with my mind controlinator!")
+print("Although on second thoughts I had a secret passphrase made just in case I needed to scrape the plan")
+password = input("Do you know the phrase? Enter: ")
+if (password=="abort12345"):
+    print("Plan aborted! Here is your flag!")
+else:
+    print("The plan must go on!")
